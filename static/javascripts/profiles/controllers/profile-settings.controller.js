d3ca4217745c14ce34aaea6ec04ee84b9ca34c22
panikt <paniktesniyom@Paniks-MacBook-Pro.local>
2017-12-10 17:40:11 +0700

    xx

--- static/javascripts/profiles/controllers/profile-settings.controller.js
+++ static/javascripts/profiles/controllers/profile-settings.controller.js
@@ -43,11 +43,12 @@
     // The "then" callback function provides the google.maps object.
         uiGmapGoogleMapApi.then(function(maps) {
              console.log('Google Maps loaded');
-             
+             $scope.render = false;
              $timeout(function () {
                console.log('Google Maps loaded222');
+               $scope.render = true;
 
-               google.maps.event.trigger(maps, 'resize');
+            //  maps.event.trigger( 'resize');
              }, 5000);
           //
         });
